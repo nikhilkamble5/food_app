6960c468c55b8221cb3ab8311b03da5b76f0ae13
nikhilkamble5 <nikhilkamble19985@gmail.com>
2024-09-24 22:44:01 +0530

    hamburger-icon added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,29 +1,34 @@
-import React from 'react'
+import React from "react";
 import { FaCartShopping } from "react-icons/fa6";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Header = () => {
   return (
-    <div className='flex justify-between py-[20px] px-[25px]'>
-        
-        <div>
-            <p>Yummy <span>Food</span></p>
-        </div>
+    <div className="flex justify-between py-[20px] px-[25px] font-semibold border-b">
+      <div>
+        <p>
+          Yummy <span className="text-[#fbc531]  ">Food</span>
+        </p>
+      </div>
 
+      <div>
+        <ul className="flex gap-4">
+          <li>Home</li>
+          <li>About</li>
+          <li>offers</li>
+          <li>Sign In</li>
+        </ul>
         <div>
-            <ul className='flex gap-4'>
-                <li>Home</li>
-                <li>About</li>
-                <li>offers</li>
-                <li>Sign In</li>
-
-            </ul>
+          <GiHamburgerMenu />
         </div>
-        <div>
-            <p><FaCartShopping />
-            </p>
-        </div>
+      </div>
+      <div>
+        <p>
+          <FaCartShopping size={30} />
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
